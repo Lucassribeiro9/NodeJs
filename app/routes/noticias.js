78b653235097d283de4459415f6a242d75361409
lucas.devlop94@gmail.com <lucas.devlop94@gmail.com>
2021-03-25 16:14:02 -0300

    Criando views dinamicas com EJS

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -10,9 +10,9 @@ module.exports = function(app){
     });
 
     connection.query('select * from noticias', function(error, result) {
-        res.send(result);
+        res.render("noticias/noticias", {noticias: result});
+
     });
 
-     //   res.render("noticias/noticias");
     });
 };
